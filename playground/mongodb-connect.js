8b09ae04ef1e763438ed1ae265e72581b06428fb
masila <abelmasila@gmail.com>
2018-04-25 20:25:50 +0300

    Added ObjectID using es6 object destructuring

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,9 @@
-const MongoClient=require('mongodb').MongoClient;
+//const MongoClient=require('mongodb').MongoClient;
+const  {MongoClient, ObjectID}=require('mongodb');
+
+//const obj=new ObjectID();
+
+//console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
     if(err){
@@ -14,16 +19,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
     //     }
     //     console.log(JSON.stringify(result.ops,undefined,2));
     // });
-    db.collection('Users').insertOne({
-        name:'Abel Masila',
-        age: 25,
-        location: 'Nairobi'
-    },(err,result)=>{
-        if(err){
-            return console.log('Unable to insert user ', err);
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    // db.collection('Users').insertOne({
+    //     name:'Abel Masila',
+    //     age: 25,
+    //     location: 'Nairobi'
+    // },(err,result)=>{
+    //     if(err){
+    //         return console.log('Unable to insert user ', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined,2));
+    // });
     //close db in ther server
     db.close();
 });
