28a1f1cb73c5f156f043855a7f705dfb06b523dd
masila <abelmasila@gmail.com>
2018-04-25 20:12:46 +0300

    Added new collection

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -5,14 +5,24 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
         return console.log('Unable to connect to mongodb server')
     }
     console.log('Connected to mongodb server');
-    db.collection('Todos').insertOne({
-        text:'New todo',
-        completed: false
-    },(err, result)=>{
+    // db.collection('Todos').insertOne({
+    //     text:'New todo',
+    //     completed: false
+    // },(err, result)=>{
+    //     if(err){
+    //         return console.log("Unable to insert todo ", err);
+    //     }
+    //     console.log(JSON.stringify(result.ops,undefined,2));
+    // });
+    db.collection('Users').insertOne({
+        name:'Abel Masila',
+        age: 25,
+        location: 'Nairobi'
+    },(err,result)=>{
         if(err){
-            return console.log("Unable to insert todo ", err);
+            return console.log('Unable to insert user ', err);
         }
-        console.log(JSON.stringify(result.ops,undefined,2));
+        console.log(JSON.stringify(result.ops, undefined, 2));
     });
     //close db in ther server
     db.close();
