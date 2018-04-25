ca4a7b12ab854eb0f8b00721bb83b0d73fc8642d
masila <abelmasila@gmail.com>
2018-04-25 20:05:22 +0300

    Added one document and new mongodb driver version

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -5,6 +5,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
         return console.log('Unable to connect to mongodb server')
     }
     console.log('Connected to mongodb server');
+    db.collection('Todos').insertOne({
+        text:'New todo',
+        completed: false
+    },(err, result)=>{
+        if(err){
+            return console.log("Unable to insert todo ", err);
+        }
+        console.log(JSON.stringify(result.ops,undefined,2));
+    });
     //close db in ther server
     db.close();
 });
