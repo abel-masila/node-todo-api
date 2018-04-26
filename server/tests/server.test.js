c68b19da08c920ede5c4c13bdff8cbf90363db8b
masila <abelmasila@gmail.com>
2018-04-26 16:29:42 +0300

    Add GET /todos/:id test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,6 @@
 const expect=require('expect');
 const request=require('supertest');
+const {ObjectID} =require('mongodb');
 
 const {app}= require('./../server');
 const {Todo}= require('./../models/todo');
@@ -7,8 +8,10 @@ const {Todo}= require('./../models/todo');
 //add seed data
 const todos=[
     {
+        _id: new ObjectID(),
         text:"First Todo"
     },{
+        _id: new ObjectID(),
         text:"Second Todo"
     }
 ];
@@ -68,5 +71,29 @@ describe("GET /todos",()=>{
                 expect(res.body.todos.length).toBe(2);
             })
             .end(done);
+    });
+});
+
+describe("GET /todo/:id",()=>{
+    it('should return todo doc',(done)=>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo.text).toBe(todos[0].text)
+            })
+            .end(done)
+    });
+    it('should return a 404 if todo not found',(done)=>{
+         request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done)
+    });
+    it('should return 404 for non-object ids',(done)=>{
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done)
     });
 });
