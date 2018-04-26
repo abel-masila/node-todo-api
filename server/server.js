b7f7664283601eb3502d231d00c834f73c302258
masila <abelmasila@gmail.com>
2018-04-26 16:06:29 +0300

    Add GET /todos/12223

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express= require('express');
 const bodyParser=require('body-parser');
+const {ObjectID} =require('mongodb');
 
 const {mongoose}= require('./db/mongoose');
 const {Todo}= require('./models/todo');
@@ -24,6 +25,21 @@ app.get('/todos',(req,res)=>{
         res.send({todos});
     }, (e)=>{
         res.status(400).send(e);
+    });
+});
+// GET /todos/1234
+app.get('/todos/:id',(req,res)=>{
+    const id=req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    },(err)=>{
+        res.status(400).send();
     });
 });
 
