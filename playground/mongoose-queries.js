0eb75eed24c15dc69a4ac99a6527c2ea9e5b853e
masila <abelmasila@gmail.com>
2018-04-26 15:49:26 +0300

    Add queries playground

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,20 +1,33 @@
+const {ObjectID} =require('mongodb');
+
 const {mongoose}= require('./../server/db/mongoose');
 const {Todo}= require('./../server/models/todo');
 
 const id ='5ae1bf54154d72b406e98d95';
 
-Todo.find({
-    _id:id
-}).then((todos)=>{
-    console.log("Todos",todos);
-});
+if(!ObjectID.isValid(id)){
+    console.log("Id not valid!")
+}
 
-Todo.findOne({
-    _id:id
-}).then((todo)=>{
-    console.log("Todo",todo);
-});
+// Todo.find({
+//     _id:id
+// }).then((todos)=>{
+//     console.log("Todos",todos);
+// });
+
+// Todo.findOne({
+//     _id:id
+// }).then((todo)=>{
+//     console.log("Todo",todo);
+// });
 
 Todo.findById(id).then((todoById)=>{
+    if(!todoById){
+        return console.log('Id not found');
+    }
     console.log("Todo",todoById);
+}).catch((e)=>{
+    console.log(e);
 });
+
+
