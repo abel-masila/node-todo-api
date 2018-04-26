99ea56fd0109c1e4688aa29ed820ed769c2588cd
masila <abelmasila@gmail.com>
2018-04-26 14:52:48 +0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -19,6 +19,13 @@ app.post('/todos',(req,res)=>{
         res.status(400).send(err);
     });
 });
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos});
+    }, (e)=>{
+        res.status(400).send(e);
+    })
+});
 
 app.listen(3000,()=>{
     console.log('Started on port 3000');
