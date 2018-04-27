85668555e60b5d17dd4871f2f8e66f2b40be78bf
masila <abelmasila@gmail.com>
2018-04-27 12:16:05 +0300

    Add DELETE /todos/:id test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -97,3 +97,36 @@ describe("GET /todo/:id",()=>{
             .end(done)
     });
 });
+
+describe('DELETE /todos/:id',()=>{
+    it('should remove a todo',(done)=>{
+        const hexId=todos[1]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res)=>{
+                if(err){
+                    return done(err);
+                }
+                Todo.findById(hexId).then((todo)=>{
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e)=>done(e));
+            });
+    });
+    it('should return 404 if todo not found',(done)=>{
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done)
+    });
+    it('should return 404 id ObjectID is invalid',(done)=>{
+        request(app)
+        .delete('/todos/123')
+        .expect(404)
+        .end(done)
+    });
+})
