2153c512b3b3540966981497b06929b555fbfca5
masila <abelmasila@gmail.com>
2018-04-27 11:25:18 +0300

    Added Heroku config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 //tell mongoose to use promises instead of default callbacks
 mongoose.Promise=global.Promise;
 //connect to db
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 //export mongosoe
 module.exports={mongoose};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const {User}= require('./models/user');
 
 const app =express();
 
+ const port=process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
@@ -43,8 +45,8 @@ app.get('/todos/:id',(req,res)=>{
     });
 });
 
-app.listen(3000,()=>{
-    console.log('Started on port 3000');
+app.listen(port,()=>{
+    console.log(`Started up on port ${port}`);
 });
 
 module.exports={app};
