1106cc9eb6697990c848a904c474b8e11eb2b229
masila <abelmasila@gmail.com>
2018-05-03 09:12:10 +0300

    Password hashing before saving using mongoose middleware

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,27 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
-const data={
-    id:10
-};
-const token=jwt.sign(data,'123abc');
-console.log(`Token: ${token}`);
+const pass='123abc!';
+// bcrypt.genSalt(10,(err, salt)=>{
+//     bcrypt.hash(pass,salt,(err,hash)=>{
+//         console.log(hash);
+//     });
+// });
+const hashedPassword='$2a$10$zzQRhD.lcPVZnRogKH5GaO8RF81ucM6wzVUYfrvfyII7pFmuAmePq';
 
-const decoded=jwt.verify(token,'123abc');
-console.log('Decoded :', decoded);
+bcrypt.compare(pass,hashedPassword,(err,res)=>{
+    console.log(res);
+});
+
+// const data={
+//     id:10
+// };
+// const token=jwt.sign(data,'123abc');
+// console.log(`Token: ${token}`);
+
+// const decoded=jwt.verify(token,'123abc');
+// console.log('Decoded :', decoded);
 
 
 
--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const validator=require('validator');
 const jwt=require('jsonwebtoken');
 
 const _=require('lodash');
+const bcrypt=require('bcryptjs');
 
 const UserSchema=new mongoose.Schema(
     {
@@ -73,6 +74,20 @@ UserSchema.methods.toJSON = function () {
         'tokens.access':'auth'
     });
   };
+
+  UserSchema.pre('save',function(next){
+    const user=this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(user.password,salt,(err,hash)=>{
+                user.password=hash;
+                next();
+            });
+        });
+    } else{
+        next();
+    }
+  });
   
 const User= mongoose.model('User',UserSchema);
 
