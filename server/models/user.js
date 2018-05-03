7f7595097904a9c90c1bfe2773acb4b8c8120fd0
masila <abelmasila@gmail.com>
2018-05-03 13:29:15 +0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -75,6 +75,24 @@ UserSchema.methods.toJSON = function () {
     });
   };
 
+  UserSchema.statics.findByCredentials=function(email,password){
+      var User=this;
+      return User.findOne({email}).then((user)=>{
+        if(!user){
+            return Promise.reject();
+        } 
+        return new Promise((resolve, reject)=>{
+            bcrypt.compare(password,user.password,(err,res)=>{
+                if(res){
+                    resolve(user);
+                } else{
+                    reject();
+                }
+            });
+        })
+      });
+  };
+
   UserSchema.pre('save',function(next){
     const user=this;
     if(user.isModified('password')){
