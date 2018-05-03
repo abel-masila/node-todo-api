6a5f72ef9e25505c440fddfe79b5984d78974f0c
masila <abelmasila@gmail.com>
2018-05-03 08:09:20 +0300

    Added JWT token generation

--- server/models/user.js
+++ server/models/user.js
@@ -1,32 +1,61 @@
 const mongoose=require('mongoose');
 const validator=require('validator');
-const User= mongoose.model('User',{
-    email:{
-        type:String,
-        minlength: 1,
-        required:true,
-        trim: true,
-        unique:true,
-        validate:{
-            validator:validator.isEmail,
-            message:'{VALUE} is not a valide email'
-        }
-    },
-    password:{
-        type:String,
-        required:true,
-        minlength:6
-    },
-    tokens:[{
-        access:{
+const jwt=require('jsonwebtoken');
+
+const _=require('lodash');
+
+const UserSchema=new mongoose.Schema(
+    {
+        email:{
             type:String,
-            required:true
+            minlength: 1,
+            required:true,
+            trim: true,
+            unique:true,
+            validate:{
+                validator:validator.isEmail,
+                message:'{VALUE} is not a valide email'
+            }
         },
-        token:{
+        password:{
             type:String,
-            required:true
-        }
-    }]
-});
+            required:true,
+            minlength:6
+        },
+        tokens:[{
+            access:{
+                type:String,
+                required:true
+            },
+            token:{
+                type:String,
+                required:true
+            }
+        }]
+    },
+    { usePushEach: true }
+);
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+  
+    return _.pick(userObject, ['_id', 'email']);
+  };
+  
+  // Instance methods
+  UserSchema.methods.generateAuthToken = function () {
+    var user = this; // individual document
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(),access},'123abc').toString();
+  
+    user.tokens.push({access, token});
+  
+    return user.save().then(() => {
+      return token; // We can chain a promise to this return
+    });
+  };
+  
+const User= mongoose.model('User',UserSchema);
 
 module.exports={User};
--- server/server.js
+++ server/server.js
@@ -89,13 +89,19 @@ app.patch('/todos/:id', (req,res)=>{
 
 //POST /users
 app.post('/users',(req,res)=>{
-    var body=_.pick(req.body,['email','password']);
-    const user=new User(body);
-    user.save().then((user)=>{
-        res.status(200).send({user});
-    },(err)=>{
-        res.status(404).send(err);
-    });
+     // allow only a subset of User model to be accessible by a user
+    var body = _.pick(req.body, ['email', 'password']);
+
+    // create a new User model
+    var user = new User(body);
+    // save the new model to the DB
+   user.save().then(()=>{
+        return user.generateAuthToken();
+   }).then((token)=>{
+        res.header("x-auth",token).send(user);
+   }).catch((e)=>{
+       res.status(400).send(e);
+   })
 });
 app.listen(port,()=>{
     console.log(`Started up on port ${port}`);
