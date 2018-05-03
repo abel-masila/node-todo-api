96fe1417a4dd62de3b42a070d95bf567f2126aba
masila <abelmasila@gmail.com>
2018-05-03 10:02:07 +0300

    Add tests for GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,6 +4,7 @@ const {ObjectID} =require('mongodb');
 
 const {app}= require('./../server');
 const {Todo}= require('./../models/todo');
+const {User}= require('./../models/user');
 
 const {todos,seedTodos, users,seedUsers}=require('./seed/seed');
 
@@ -149,5 +150,70 @@ describe("PATCH  /todos/:id", ()=>{
                 expect(res.body.todo.completedAt).toNotExist();
             })
             .end(done);
+    });
+});
+
+describe('GET /users/me',()=>{
+    it('should return if authenticated',(done)=>{
+        request(app)
+            .get('/users/me')
+            .set('x-auth',users[0].tokens[0].token)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body._id).toBe(users[0]._id.toHexString());
+                expect(res.body.email).toBe(users[0].email);
+            })
+            .end(done);
+    });
+    it('should return a 401 if not authenticated',(done)=>{
+        request(app)
+            .get('/users/me')
+            .expect(401)
+            .expect((res)=>{
+                expect(res.body).toEqual({});
+            })
+            .end(done)
+    });
+});
+describe('POST /users',()=>{
+    const email="abel4@gmail.com";
+    const password="123457";
+
+    it('should create a user',(done)=>{
+        request(app)
+            .post('/users')
+            .send({email,password})
+            .expect(200)
+            .expect((res)=>{
+                expect(res.headers['x-auth']).toExist();
+                expect(res.body._id).toExist();
+                expect(res.body.email).toBe(email);
+            })
+            .end((err)=>{
+                if(err){
+                    return done(err);
+                }
+                User.findOne({email}).then((user)=>{
+                    expect(user).toExist();
+                    expect(user.password).toNotBe(password);
+                    done();
+                });
+            });
+    });
+    it('it should validation errors if request is invalid',(done)=>{
+        const badEmail="abel";
+        const badPassword="123";
+        request(app)
+            .post('/users')
+            .send({badEmail,badPassword})
+            .expect(400)
+            .end(done)
+    });
+    it('should not create a user if email is in use',(done)=>{
+        request(app)
+            .post('/users')
+            .send({email: users[0].email, password:'123'})
+            .expect(400)
+            .end(done)
     });
 });
